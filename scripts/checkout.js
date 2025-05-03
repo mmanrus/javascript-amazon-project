6b5394c65e7f08f9e8f573c3e4a28685b5ccbd99
mmanrus <mmanrusiana@gmail.com>
2025-05-03 18:46:09 +0800

    Deleting checked out items from DOM when clicking delete.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,7 +10,7 @@ cartModule.cart.forEach((cartItem)=>{
      const itemInstance = productById[productId];
      
      cartHTML += `
-               <div class="cart-item-container js-cart-item">
+               <div class="cart-item-container js-cart-item js-cart-item-container-${itemInstance.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,6 +96,8 @@ document.querySelectorAll('.js-delete-link')
       link.addEventListener('click', ()=>{
           const productId = link.dataset.productId;
           cartModule.removeFromCart(productId);
-          
+          const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+          container.remove();
      });
 });

5b319b1648a66a3e76b8f623b123636c4a314787
manuel <mmanrusiana@gmail.com>
2025-05-05 08:51:20 +0800

    adding tests for formatCurrency

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,4 @@
 export function formatCurrency (cents){
      return (cents / 100).toFixed(2);
-     
 }
 
---
+++ tests/moneyTest.js
@@ -0,0 +1,7 @@
+import { formatCurrency } from "../scripts/utils/money";
+
+if(formatCurrency(2095) === '20.95') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}

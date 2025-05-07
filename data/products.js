da0807305c9474097b86c05d6a1a93d8bb6bc194
manuel <mmanrusiana@gmail.com>
2025-05-07 21:29:53 +0800

    convert products object into a Product Class, added Clothing that inherit Product and used inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -1,3 +1,46 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl () {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+  extraInfoHTML() {
+    return '';
+  }
+}
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails); // calls the constructor of the parent class which is the product
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    // super.extraInfoHTML()
+    return `<a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`;
+  }
+}
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -655,9 +698,15 @@ export const products = [
       "hoodies",
       "apparel",
       "mens"
-    ]
+    ] 
   }
-];
+].map((productDetails)=>{
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+});;
+
 
 export const productById = products.reduce((acc, product) =>{
   acc[product.id] = product;
